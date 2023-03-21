f8c608afd21b2580c04e83a9fdf0a62fc0b416ed
Galmoli <tonigalmes98@gmail.com>
2023-03-21 12:34:58 +0100

    add ARB and DISTRIBUTOR addy

--- scripts/tracker.js
+++ scripts/tracker.js
@@ -1,4 +1,6 @@
 const hre = require("hardhat");
+const ARB = "0x912CE59144191C1204E64559FE8253a0e49E6548";
+const DISTRIBUTOR = "0x67a24CE4321aB3aF51c2D0a4801c3E111D88C9d9"; 
 
 async function main() {
   

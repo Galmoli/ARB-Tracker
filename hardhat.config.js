ed1c56c30648c05b41a5e02a7cb79969c7fb39ad
Galmoli <tonigalmes98@gmail.com>
2023-03-21 12:47:08 +0100

    Add contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.7",
+        version: "0.8.16",
         settings: {
           optimizer: {
             enabled: true,

70081c5ba5417503b6f58e6dddac8bbb0debb1f1
Galmoli <tonigalmes98@gmail.com>
2023-03-21 12:32:18 +0100

    Project init

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  defaultNetwork: "hardhat",
+    networks: {
+      hardhat: {
+        forking: {
+          url: process.env.ARB,
+        }
+      },
+      arbitrum: {
+        chainId: 42161,
+        url: process.env.ARB,
+      }
+    },
+};
